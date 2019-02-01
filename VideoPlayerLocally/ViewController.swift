a3144678cb82e21150269578dc07422ce4169307
Siddharth <sathesiddharth14@gmail.com>
2019-02-01 14:07:48 +0530

    Initial Commit

---
+++ VideoPlayerLocally/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VideoPlayerLocally
+//
+//  Created by siddharth on 01/02/19.
+//  Copyright © 2019 clarionTechnologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
